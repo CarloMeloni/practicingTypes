215a200fdb5379b0b65df785440730d915311d9f
carloMeloni <carlo.meloni@hotmail.it>
2020-07-11 16:30:04 +0200

    Solo committando

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-console.log('TYPESCRIPPTE!');
+function add(n1, n2) {
+    return n1 + n2;
+}
+var num1 = 5;
+var num2 = 3;
+var sum = add(num1, num2);
+console.log(sum);
--- app.ts
+++ app.ts
@@ -1,3 +1,9 @@
-console.log('Ecco github!');
-console.log('Ecco github!');
-console.log('Ecco github!');
+function add(n1: number, n2: number) {
+    return n1 + n2;
+}
+
+const num1 = 5;
+const num2 = 3;
+
+const sum = add(num1, num2);
+console.log(sum);
