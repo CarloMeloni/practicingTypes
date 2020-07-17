589321678b06f6de5dff6e7c28c4bece1875477e
carloMeloni <carlo.meloni@hotmail.it>
2020-07-17 16:22:51 +0200

    Finishing drag&drop

--- cl-interfaces-01-starting-setup/cl-interfaces-01-starting-setup/src/app.ts
+++ cl-interfaces-01-starting-setup/cl-interfaces-01-starting-setup/src/app.ts
@@ -64,6 +64,18 @@ class ProjectState extends State<Project> {
             ProjectStatus.Active
         );
         this.projects.push(newProject);
+        this.updateListeners();
+    }
+
+    moveProject(projectId: string, newStatus: ProjectStatus) {
+        const project = this.projects.find(prj => prj.id === projectId);
+        if(project && project.projectStatus !== newStatus) {
+            project.projectStatus = newStatus;
+            this.updateListeners();
+        }
+    }
+
+    private updateListeners() {
         for(const listenerFn of this.listeners) {
             listenerFn(this.projects.slice())
         }
@@ -228,8 +240,10 @@ class ProjectList extends Component<HTMLDivElement, HTMLElement> implements Drag
         
     };
 
+    @autobind
     dropHandler(event: DragEvent) {
-        console.log(event.dataTransfer!.getData('text/plain'));
+        const prjId = (event.dataTransfer!.getData('text/plain'));
+        projectState.moveProject(prjId, this.type === 'active' ? ProjectStatus.Active : ProjectStatus.Finished )
     };
 
     @autobind
